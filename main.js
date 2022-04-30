9a5a2a4451d57c709f7a524095287777bf02ddb0
Tech Hacker Ak <92919907+Tech-Hacker-Ak@users.noreply.github.com>
2022-04-30 13:18:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function change()
+{
+  var name = document.getElementById("akname");
+  
+name.classList.toggle("akchange");
+  
+}
+function submit()
+{
+  alert("Successfully Submitted!");
+  location.reload();
+}
